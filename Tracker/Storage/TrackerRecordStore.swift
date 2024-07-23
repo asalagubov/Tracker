ba5059d6a8bbca720d59f54160a66ac503e1b6fb
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-07-23 12:55:52 +0300

    sprint_15: Added Storage and CoreData to AppDelegate

---
+++ Tracker/Storage/TrackerRecordStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerRecordStore.swift
+//  Tracker
+//
+//  Created by Alexander Salagubov on 23.07.2024.
+//
+
+import Foundation
---
+++ Tracker/Storage/TrackerStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerStore.swift
+//  Tracker
+//
+//  Created by Alexander Salagubov on 23.07.2024.
+//
+
+import Foundation
