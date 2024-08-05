4f36783616a8654d5ef5704b74a9bda581c14306
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-08-05 23:36:44 +0300

    sprint_16: Added Onboarding files

---
+++ Tracker/Onboarding/OnboardingPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingPageViewController.swift
+//  Tracker
+//
+//  Created by Alexander Salagubov on 05.08.2024.
+//
+
+import Foundation
+import UIKit
+
+final class OnboardingPageViewController: UIPageViewController {
+
+}
---
+++ Tracker/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.swift
+//  Tracker
+//
+//  Created by Alexander Salagubov on 05.08.2024.
+//
+
+import Foundation
+import UIKit
+
+final class OnboardingViewController: UIViewController {
+
+}
